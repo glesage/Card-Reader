6c71bb1572d954706be9ca342dbad40fe6e29735
Geoffroy Lesage <gefthefrench@gmail.com>
2015-07-12 01:27:40 -0500

    Initial app commit

---
+++ cardreader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cardreader
+//
+//  Created by Geoffroy Lesage on 7/12/15.
+//  Copyright (c) 2015 Geoffroy Lesage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cardreader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cardreader
+//
+//  Created by Geoffroy Lesage on 7/12/15.
+//  Copyright (c) 2015 Geoffroy Lesage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
